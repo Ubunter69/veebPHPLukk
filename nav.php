ee994475a886c1e7fa6c5d8abb67ef2f77c472d0
MarekLukk <mareklukk8@gmail.com>
2026-01-29 16:14:46 +0200

    Lisatud aja funktsioonid, riploend ja uuendatud style.css

--- nav.php
+++ nav.php
@@ -1,8 +1,16 @@
-<nav>
-    <ul>
-        <li><a href="index.php" class="nav-link">Avaleht</a></li>
-        <li><a href="?link=gitKasutamine.php" class="nav-link">Git käsud</a></li>
-        <li><a href="?link=too1.php" class="nav-link">PHP Tööd</a></li>
-        <li><a href="https://mareklukk24.thkit.ee/" class="nav-link">vana index</a></li>
-    </ul>
-</nav>
+<div>
+
+    <nav>
+        <ul>
+            <li><a href="index.php" class="nav-link">Avaleht</a></li>
+            <li><a href="?link=gitKasutamine.php" class="nav-link">Git käsud</a></li>
+            <li class="dropdown">
+                <a href="">PHP töö</a>
+                <ul class="dropdown-menu">
+                    <li><a href="?link=ajaFunktsioonid.php" class="nav-link">Ajafunktsioonid</a></li>
+                </ul>
+            </li>
+            <li><a href="https://mareklukk24.thkit.ee/" class="nav-link">vana index</a></li>
+        </ul>
+    </nav>
+</div>
